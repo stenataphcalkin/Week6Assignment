259d08014ff3dbd73c4793aab8d7d52c37e5c0a1
Steph <steph@polyhydral.co.uk>
2025-09-21 11:43:17 +0100

    Gallery start

--- week6assignment/src/App.jsx
+++ week6assignment/src/App.jsx
@@ -3,34 +3,13 @@
 // - Start with your wireframe: you build your React client based on the UI
 
 import "./App.css";
-// import "./week6assignment/src/components/gallery/gallery.jsx" Not working. Brain hurt. Moving on.
+import gallery from "./components/gallery";
 
-import { useState, useEffect} from "react" 
-export default function App() {
-  const [images, setImages] = useState([]);
-
-  useEffect(() => {
-    async function fetchImages() {
-      let response = await fetch(
-        "https://week-6-api.vercel.app/api/images"
-      );
-      let data = await response.json();
-      setImages(data);
-    }
-    fetchImages();
-  }, []);
-
-  return (
-    <div>
-      <h1>Images</h1>
-      <ul>
-        {images.map((image) => (
-          <ul key={image.id}>{image.title}&gt;{image.url}&gt;{image.alt}</ul>
-        ))}
-      </ul>
-    </div>
-  );
+export default function App () {
+  <gallery />
 }
+
+
 
 // export default function App() {
   //state
